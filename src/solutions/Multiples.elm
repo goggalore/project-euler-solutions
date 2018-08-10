9eadfd76f5ccfc07b8acab370c735cb3f017893b
goggalore <junec177@gmail.com>
2018-08-10 15:59:20 -0600

    add comment in Multiples.elm that explains functionality

--- src/solutions/Multiples.elm
+++ src/solutions/Multiples.elm
@@ -3,6 +3,9 @@ module Multiples exposing (sum)
 import Tuple
 
 
+-- sums any multiples of numbers provided as a list within a range
+-- so sum [3, 5] (0, 15) will sum 3, 5, 6, 9, 12, and 15
+
 sum : List Int -> ( Int, Int ) -> Int
 sum nums range =
     List.range (Tuple.first range) (Tuple.second range)
